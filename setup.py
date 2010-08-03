3a7bc2f294784141309fd10e805e8f84de1bd043
Alex Lee <lee@ccs.neu.edu>
2010-08-03 00:34:35 -0700

    Version bump to 0.2.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name='xmpp2',
-    version='0.1',
+    version='0.2',
     description='An XMPP client',
     long_description='An XMPP client',
     author='Alex Lee',
