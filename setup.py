a2f248221e5510e56171470349180e2cb13f9038
Alex Lee <lee@ccs.neu.edu>
2010-08-03 01:14:52 -0700

    Add classifiers. Fix MANIFEST.in

--- setup.py
+++ setup.py
@@ -10,6 +10,16 @@ setup(
     long_description='An XMPP client',
     author='Alex Lee',
     author_email='xmpp2@thirdbeat.com',
+    maintainer='Alex Lee',
+    maintainer_email='xmpp2@thirdbeat.com',
     url='http://xmpp2.thirdbeat.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers'
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Communications :: Chat',
+        'Topic :: Software Development :: Libraries',
+    ],
     packages=['xmpp2', 'xmpp2.handler']
 )
